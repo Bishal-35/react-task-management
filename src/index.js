f2c00da5001696b77e0039ecebdc6d7f790199e2
Bishal-35 <karmakarbishal56@gmail.com>
2025-04-01 22:04:14 +0530

    fixed redux store issue

--- src/index.js
+++ src/index.js
@@ -11,11 +11,9 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <Provider store={store}>
       <CssBaseline />
       <App />
     </Provider>
-  </React.StrictMode>
 );
 
